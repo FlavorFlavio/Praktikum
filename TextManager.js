f6ec37ccc8798390bbdf80fbf25165eeeb0a5a69
FlavorFlavio <f_beckmann@hotmail.de>
2020-03-20 11:48:36 +0100

    Delete Div

--- TextManager.js
+++ TextManager.js
@@ -1,16 +1,19 @@
 
-
+var counter=0;
 function AddText(string){
-    console.log('test '+string);
+    console.log(counter+' addText '+string);
     var div = document.createElement("div");
+    div.id= "div"+counter;
     
       var textBox = document.createElement("p");
 
       var buttonWork = document.createElement("button");
+      buttonWork.textContent='Edit';
+      
       var umbruch = document.createElement("br");
       var buttonDelete = document.createElement("button");
-      
-
+      buttonDelete.textContent='Delete';
+      buttonDelete.setAttribute('onclick', 'DeleteDiv(document.getElementById("div'+counter+'"))')
       var text = document.createTextNode(string);
       textBox.appendChild(text);
       //text.AddText = string;
@@ -22,5 +25,36 @@ function AddText(string){
       div.appendChild(umbruch);
       div.appendChild(textBox);
       parentDiv.appendChild(div);
+counter++;
+}
+
+
+function EditDiv(element){
+
+
+    console.log('Editiv '+element);
+    
+   var  parentDiv= document.getElementById("ParentDiv");
+   parentDiv.removeChild(element);
+   // parentDiv.removeChild
+   // for (var i = 0; i < 9; i++) {
+  //      console.log(i +' '+parentDiv.childNodes[i]);
+  //    if(parentDiv.childNodes[i] == element){
+//
+}
+function DeleteDiv(element){
+    console.log('deleteDiv '+element);
+    
+   var  parentDiv= document.getElementById("ParentDiv");
+   parentDiv.removeChild(element);
+   // parentDiv.removeChild
+   // for (var i = 0; i < 9; i++) {
+  //      console.log(i +' '+parentDiv.childNodes[i]);
+  //    if(parentDiv.childNodes[i] == element){
+   //   }
+    // }
+    
+
+
 
 }
