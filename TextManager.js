488e823b92e350d0ded3152368285dca69326818
FlavorFlavio <f_beckmann@hotmail.de>
2020-03-20 13:38:18 +0100

    editieren der divs (Text umändern)

--- TextManager.js
+++ TextManager.js
@@ -2,59 +2,70 @@
 var counter=0;
 function AddText(string){
     console.log(counter+' addText '+string);
+
+    var parentDiv = document.getElementById("ParentDiv");
+    
     var div = document.createElement("div");
     div.id= "div"+counter;
     
       var textBox = document.createElement("p");
+      textBox.id="textdiv"+counter;
+      var text = document.createTextNode(string);
+      var umbruch = document.createElement("br");
 
       var buttonWork = document.createElement("button");
       buttonWork.textContent='Edit';
+      buttonWork.setAttribute('onclick', 'EditDiv(document.getElementById("div'+counter+'"))')
       
-      var umbruch = document.createElement("br");
       var buttonDelete = document.createElement("button");
       buttonDelete.textContent='Delete';
+      buttonDelete.id= "DeleteButtondiv"+counter;;
       buttonDelete.setAttribute('onclick', 'DeleteDiv(document.getElementById("div'+counter+'"))')
-      var text = document.createTextNode(string);
+
+      counter++;
+
       textBox.appendChild(text);
-      //text.AddText = string;
-  
-      
-      var parentDiv = document.getElementById("ParentDiv");
       div.appendChild(buttonWork);
       div.appendChild(buttonDelete);
       div.appendChild(umbruch);
       div.appendChild(textBox);
       parentDiv.appendChild(div);
-counter++;
+
 }
 
 
 function EditDiv(element){
-
-
-    console.log('Editiv '+element);
+    console.log('EditDiv '+element.id);
     
-   var  parentDiv= document.getElementById("ParentDiv");
-   parentDiv.removeChild(element);
-   // parentDiv.removeChild
-   // for (var i = 0; i < 9; i++) {
-  //      console.log(i +' '+parentDiv.childNodes[i]);
-  //    if(parentDiv.childNodes[i] == element){
-//
+    var inputfield = document.createElement("INPUT");
+    inputfield.id= 'inputfield'+ element.id;
+    inputfield.setAttribute("type", "text");
+    element.appendChild(inputfield);
+
+    var buttonDelete= document.getElementById('DeleteButton'+element.id);
+    buttonDelete.setAttribute('onclick', 'EditText("'+element.id+'")')
+    buttonDelete.textContent= 'Save new text';
 }
+function EditText(elementId){
+    
+    console.log('EditText '+elementId);
+    
+    var div= document.getElementById(elementId);
+
+    var deleteButton= document.getElementById('DeleteButton'+elementId);
+    deleteButton.textContent= 'Delete';
+    deleteButton.setAttribute('onclick', 'DeleteDiv(document.getElementById("'+elementId+'"))')
+    
+    var inputfield= document.getElementById('inputfield'+elementId);
+    var text= document.getElementById('text'+elementId);
+    text.textContent=inputfield.value;
+
+    div.removeChild(inputfield);
+}
+
 function DeleteDiv(element){
     console.log('deleteDiv '+element);
     
-   var  parentDiv= document.getElementById("ParentDiv");
-   parentDiv.removeChild(element);
-   // parentDiv.removeChild
-   // for (var i = 0; i < 9; i++) {
-  //      console.log(i +' '+parentDiv.childNodes[i]);
-  //    if(parentDiv.childNodes[i] == element){
-   //   }
-    // }
-    
-
-
-
+    var  parentDiv= document.getElementById("ParentDiv");
+    parentDiv.removeChild(element);
 }
